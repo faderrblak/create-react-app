66cfead5cdfea84ded0ed4058eefc7bfffd89e4c
johmfaderr <johmfaderr@gmail.com>
2020-06-15 22:00:04 +0300

    Updates

---
+++ precache-manifest.cc5caccd07e24a4f24d787fa1bc4ad15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655421d8ff72a28bb3a883bc7685e2e0",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "c0685d8d78643084a645",
+    "url": "/create-react-app/static/js/2.5cfc9fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.5cfc9fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e42f329267a3d06042",
+    "url": "/create-react-app/static/js/main.ad62cf67.chunk.js"
+  },
+  {
+    "revision": "03a4a16aad00818d548b",
+    "url": "/create-react-app/static/js/runtime-main.e6070df4.js"
+  }
+]);
